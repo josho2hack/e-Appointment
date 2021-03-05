fa6df9c81499dd1027ebec959b9b8ea7af92e0e1
Josho2 Hack <josho2hack@gmail.com>
2021-03-05 22:08:02 +0700

    update link vue again

--- resources/js/Components/ApplicationLogo.vue
+++ resources/js/Components/ApplicationLogo.vue
@@ -1,13 +1,3 @@
 <template>
-    <img class="rd-logo" src="<%= BASE_URL %>assets/images/e-appointment.png" />
+    <img class="rd-logo" src="/e-appointment/public/assets/images/e-appointment.png" />
 </template>
-
-<script>
-export default {
-    data() {
-        return {
-            publicPath: process.env.BASE_URL,
-        };
-    },
-};
-</script>
