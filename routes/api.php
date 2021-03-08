87f5cc6eb01db31a153f62729f4c2e12d80cf063
Josho2 Hack <josho2hack@gmail.com>
2021-03-08 22:00:44 +0700

    update api nid

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('/nid/{nid}', function ($nid) {
-    $response = Http::post(env('EOFFICE_AUTH'));
+    $response = Http::post(env('NID_SERVICE'));
     $data = $response->json();
     return response()->json($data, 200, [], JSON_NUMERIC_CHECK);
 });
