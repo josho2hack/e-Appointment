352b3263bea715b94d94aade0d7061ab352fe5ce
Josho Hack <josho2hack@gmail.com>
2021-03-15 10:46:38 +0700

    debug

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -270,13 +270,13 @@ class BookingController extends Controller
         $user_filter = array_filter($user, function($obj) {
             if (isset($obj->DataUser)) {
                 foreach ($obj->DataUser as $u) {
-                    if ($u->EMPTYPE !== "1") return false;
+                    if ($u->EMPTYPE != 1) return false;
                 }
             }
             return true;
         });
 
-        dd($user_filter);
+        dd($user,$user_filter);
 
         return Inertia::render('Booking/Edit', [
             'appointment' => $booking->appointment,
