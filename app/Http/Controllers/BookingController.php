1057350a7acc0b5dc22d402dbb00a724e4ef4873
Josho Hack <josho2hack@gmail.com>
2021-03-15 10:55:24 +0700

    debug

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -264,7 +264,7 @@ class BookingController extends Controller
                 'checkPass' => env('EOFFICE_CHKPASS'),
                 'officeId' => Auth::user()->office->code,
             ]);
-            $user = $response;
+            $user = $response->array();
         }
 
         $user_filter = array_filter($user, function($obj) {
