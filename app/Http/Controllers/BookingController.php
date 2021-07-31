20be3365ee14fbd975f6a25d0e288bdf7255625b
Josho2 Hack <josho2hack@gmail.com>
2021-07-31 14:59:30 +0700

    update debug

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -269,7 +269,7 @@ class BookingController extends Controller
                 'officeId' => $booking->employee != null ? $booking->employee->office->code : $booking->appointment->office->code,
             ]);
 
-            dd($booking->employee,$booking->employee->office->code,$booking->appointment->office->code);
+            //dd($booking->employee,$booking->employee->office->code,$booking->appointment->office->code);
 
             $user = $response->json("DataUser");
 
