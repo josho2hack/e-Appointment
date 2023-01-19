c3f26599ce8b1316f369bda0b1cb7d684af5dba0
supansa <supansa@DESKTOP-PBVLIO4>
2023-01-19 14:47:03 +0700

    Nid + email

--- app/Http/Controllers/NidController.php
+++ app/Http/Controllers/NidController.php
@@ -9,10 +9,13 @@ class NidController extends Controller
 {
     public function getInfo($nid)
     {
+    /*
         $response = Http::timeout(3)->withOptions([
-            'proxy' => env('PROXY'),
+            //'proxy' => env('PROXY'),
             //'timeout' => 3 //second
         ])->get(env('NID_SERVICE') . $nid);
+        */
+        $response = Http::timeout(3)->get(env('NID_SERVICE') . $nid);
 
         //dd(env('PROXY'),env('NID_SERVICE'));
 
--- config/mail.php
+++ config/mail.php
@@ -43,6 +43,13 @@ return [
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
             'auth_mode' => null,
+            'stream' => [
+                'ssl' => [
+                    'allow_self_signed' => true,
+                    'verify_peer' => false,
+                    'verify_peer_name' => false,
+                ],
+            ],
         ],
 
         'ses' => [
@@ -84,7 +91,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'appointment@rd.go.th'),
+        'address' => env('MAIL_FROM_ADDRESS', 'e-appointment@rd.go.th'),
         'name' => env('MAIL_FROM_NAME', 'e-Appointment'),
     ],
 
